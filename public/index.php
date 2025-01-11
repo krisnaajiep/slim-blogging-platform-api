5755438a85e90222d9eff02d4cfeaf209552d5c7
Krisna Ajie <krisnaajiep@gmail.com>
2025-01-11 16:00:06 +0700

    chore(setup): initialize project with Slim 4
    - Install Slim 4 via Composer
    - Create basic directory structure
    - Add .gitignore

---
+++ public/index.php
@@ -0,0 +1,25 @@
+<?php
+
+use Slim\Factory\AppFactory;
+use Middlewares\TrailingSlash;
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+
+require __DIR__ . '/../vendor/autoload.php';
+
+$app = AppFactory::create();
+
+$app->addRoutingMiddleware();
+
+$app->addErrorMiddleware(true, true, true)
+	->getDefaultErrorHandler()
+	->forceContentType('application/json');
+
+$app->add(new TrailingSlash(trailingSlash: false));
+
+$app->get('/', function (Request $request, Response $response, $args) {
+	$response->getBody()->write('Hello World');
+	return $response;
+});
+
+$app->run();
