fa6f13e7e02be51b23658ae3fa0dadd812ce42d1
Krisna Ajie <krisnaajiep@gmail.com>
2025-01-11 23:58:47 +0700

    feat(post): add create blog post feature
    - Add create method in app/Models/Post.php and app/Controllers/PostController.php
    - Add post route in public/index.php to create blog post

--- app/Controllers/PostController.php
+++ app/Controllers/PostController.php
@@ -15,4 +15,23 @@ class PostController
     {
         $this->model = new Post();
     }
+
+    public function create(Request $request, Response $response, $args): ResponseInterface
+    {
+        $data = $request->getParsedBody();
+
+        $post = $this->model->create($data);
+
+        $post['tags'] = json_decode($post['tags']);
+
+        $created_at = new \DateTime($post['created_at']);
+        $updated_at = new \DateTime($post['updated_at']);
+
+        $post['created_at'] = $created_at->format('Y-m-d\TH:i:s\Z');
+        $post['updated_at'] = $updated_at->format('Y-m-d\TH:i:s\Z');
+
+        $response->getBody()->write(json_encode($post));
+
+        return $response->withStatus(201);
+    }
 }
