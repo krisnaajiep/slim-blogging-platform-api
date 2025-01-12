22d464efbd68e399f2c76eba653ff7a5941c3bae
Krisna Ajie <krisnaajiep@gmail.com>
2025-01-12 16:41:01 +0700

    chore(route): delete hello world route

--- public/index.php
+++ public/index.php
@@ -3,8 +3,6 @@
 use Slim\Factory\AppFactory;
 use Middlewares\TrailingSlash;
 use App\Controllers\PostController;
-use Psr\Http\Message\ResponseInterface as Response;
-use Psr\Http\Message\ServerRequestInterface as Request;
 use App\Middlewares\JsonBodyParserMiddleware;
 
 require __DIR__ . '/../vendor/autoload.php';
@@ -23,11 +21,6 @@ $app->addErrorMiddleware(true, true, true)
 $app->add(new TrailingSlash(trailingSlash: false));
 
 $app->add(new JsonBodyParserMiddleware());
-
-$app->get('/', function (Request $request, Response $response, $args) {
-	$response->getBody()->write('Hello World');
-	return $response;
-});
 
 $app->post('/posts', [PostController::class, 'create']);
 $app->get('/posts/{id}', [PostController::class, 'show']);
