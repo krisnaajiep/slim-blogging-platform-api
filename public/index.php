115e48505f0f1de5f90e4058f8c9187e205da782
Krisna Ajie <krisnaajiep@gmail.com>
2025-01-12 16:01:13 +0700

    feat(post): add update blog post feature
    - Add method to update blog post in PostController
    - Add method to update blog post in Post model
    - Add route to update blog post in index.php

--- public/index.php
+++ public/index.php
@@ -32,5 +32,6 @@ $app->get('/', function (Request $request, Response $response, $args) {
 $app->post('/posts', [PostController::class, 'create']);
 $app->get('/posts/{id}', [PostController::class, 'show']);
 $app->get('/posts', [PostController::class, 'index']);
+$app->put('/posts/{id}', [PostController::class, 'update']);
 
 $app->run();
