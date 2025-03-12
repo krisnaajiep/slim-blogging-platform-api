1c63944b3e663899e03c240b3d84a13a39624159
Krisna Ajie <krisnaajiep@gmail.com>
2025-03-12 23:04:54 +0700

    feat(middleware): add TrimInputMiddleware to trim input data and reorganize body parsing

--- app/middleware.php
+++ app/middleware.php
@@ -2,9 +2,14 @@
 
 use Slim\App;
 use Middlewares\TrailingSlash;
+use App\Middlewares\TrimInputMiddleware;
 use App\Middlewares\ReturningJsonMiddleware;
 
 return function (App $app) {
     $app->add(new TrailingSlash(trailingSlash: false));
+    $app->add(new TrimInputMiddleware());
     $app->add(new ReturningJsonMiddleware());
+
+    // Parse json, form data and xml
+    $app->addBodyParsingMiddleware();
 };
